37d2fd6a9dff937f6809c172d342a9372f2a62b3
Nicolas <nicolas.gamel12450@gmail.com>
2019-12-05 10:41:50 +0100

    back initialisation

---
+++ back/src/main/java/fr/team/benks/back/App.java
@@ -0,0 +1,13 @@
+package fr.team.benks.back;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
