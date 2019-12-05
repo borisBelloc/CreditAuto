61d15187498c3c4d7cc7319a0dacc4a21f5e3163
Estelle <estelle.idee@gmail.com>
2019-12-05 14:50:46 +0100

    services package and CalculService class created

---
+++ back/src/main/java/CalculService.java
@@ -0,0 +1,17 @@
+
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class CalculService {
+
+	public BigDecimal calcul(BigDecimal txAnnuel, BigDecimal nbEch, BigDecimal mtEmp) {
+
+		MathContext mc = new MathContext(4, RoundingMode.HALF_UP);
+		
+		return null;
+
+	}
+
+}
