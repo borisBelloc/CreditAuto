aa3ad7ea248b772e1b6f1934b2bc464f94021d5a
Boris BELLOC <bb.itwork@gmail.com>
2019-12-06 09:59:41 +0100

    started remove service

--- front/src/app/navigation/navbar/navbar.component.ts
+++ front/src/app/navigation/navbar/navbar.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { NavbardisplayService } from '../services/navbardisplay.service';
-
-
 
 @Component({
   selector: 'app-navbar',
@@ -12,20 +8,10 @@ import { NavbardisplayService } from '../services/navbardisplay.service';
 export class NavbarComponent implements OnInit {
 
   isCollapsed = true;
-  navbarDisplay: Observable<boolean>;
 
-  // Service ne sert que pour masquer la navbar ?
-  // appel du service (injection)
-  // constructor(private navbardisplayService: NavbardisplayService) { }
   constructor() { }
 
   ngOnInit() {
-    // this.navbarDisplay = this.navbardisplayService.onChangeDisplay();
   }
-
-  // toto(){
-  //   console.log('aaaa');
-  //   this.isCollapsed = !this.isCollapsed;
-  // }
 
 }
