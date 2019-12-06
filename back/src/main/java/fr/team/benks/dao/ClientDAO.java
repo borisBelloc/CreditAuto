1a9a0fac045f019a8c2f4640bb3ec135b831f05e
Nicolas <nicolas.gamel12450@gmail.com>
2019-12-06 12:12:00 +0100

    implementation du dao rate

--- back/src/main/java/fr/team/benks/dao/ClientDAO.java
+++ back/src/main/java/fr/team/benks/dao/ClientDAO.java
@@ -1,5 +1,40 @@
 package fr.team.benks.dao;
 
-public class ClientDAO {
+import java.util.List;
+import java.util.Optional;
+
+import fr.team.benks.model.User;
+
+public class ClientDAO implements DAO<User> {
+
+	@Override
+	public Optional<User> get(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(User t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(User t, String[] params) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User t) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
