988b6aa5d7e7630af67f84e3501c949180f08061
Boris BELLOC <bb.itwork@gmail.com>
2019-12-06 11:56:52 +0100

    navbar + header responsive ok

--- front/src/app/navigation/navbar/navbar.component.ts
+++ front/src/app/navigation/navbar/navbar.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  logo: string;
   isCollapsed = true;
 
-  constructor() { }
+  constructor() {
+    this.logo = 'assets/img/logo.png';
+  }
 
   ngOnInit() {
   }
