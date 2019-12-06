b5e0ce02ba1986bfe213a3fc58b69e0f384d927f
RAZAFIMANJATO <samoela.razafi@gmail.com>
2019-12-06 10:09:01 +0100

    add catalog module, body module, navbar component, logo

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationModule } from './navigation/navigation.module';
+import { BodyModule } from './body/body.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavigationModule,
+    BodyModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
