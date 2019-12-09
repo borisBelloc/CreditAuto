79400eed8cd941d7d148071b65c2fcf4e0790281
Estelle <estelle.idee@gmail.com>
2019-12-09 10:56:06 +0100

    calcul monthlyCost and TotalCost methods added, echeancier added

--- back/src/main/java/fr/team/benks/Main.java
+++ back/src/main/java/fr/team/benks/Main.java
@@ -1,6 +1,7 @@
 package fr.team.benks;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import fr.team.benks.services.CalculService;
 
@@ -8,7 +9,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		CalculService cs = new CalculService();
-		BigDecimal cost = cs.calcul(new BigDecimal(0.05), 24, new BigDecimal(10000));
+		BigDecimal cost = cs.calculTotalCost(new BigDecimal(0.05), 24, new BigDecimal(10000));
+
+		LocalDate date = LocalDate.parse("2018-11-13");
+		cs.afficherEcheancier(new BigDecimal(0.05), 24, new BigDecimal(10000), date);
+
 	}
 
 }
--- back/src/main/java/fr/team/benks/services/CalculService.java
+++ back/src/main/java/fr/team/benks/services/CalculService.java
@@ -1,40 +1,72 @@
 package fr.team.benks.services;
 
-
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.Date;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculService {
 
 	/**
 	 * 
 	 * @param txAnnuel taux annuel
-	 * @param nbEch nombre d'échanges
-	 * @param mtEmp montant de l'emprunt
+	 * @param nbEch    nombre d'échanges
+	 * @param mtEmp    montant de l'emprunt
 	 * @return BigDecimal Cost of the loan
 	 */
-	public BigDecimal calcul(BigDecimal txAnnuel, int nbEch, BigDecimal mtEmp) {
 
-		MathContext finalResultRounding = new MathContext(4, RoundingMode.HALF_UP);
-		MathContext preciseRounding = new MathContext(20, RoundingMode.HALF_UP);
-		
-		BigDecimal txPer = txAnnuel.divide(new BigDecimal(12), finalResultRounding);
-		System.out.println("txPer : "+txPer);
-		
-		BigDecimal divided = mtEmp.multiply(txPer).multiply(BigDecimal.ONE.add(txPer).pow(nbEch));
-		BigDecimal divisor = BigDecimal.ONE.subtract(BigDecimal.ONE.add(txPer).pow(nbEch, preciseRounding));
-		BigDecimal mtEch = divided.divide(divisor, finalResultRounding);
-		
-//		BigDecimal mtEch = (mtEmp.multiply(txPer).multiply((txPer.add(new BigDecimal(1))).pow(nbEch, preciseRounding)))
-//				.divide(new BigDecimal(1).subtract((txPer.add(new BigDecimal(1))).pow(-nbEch, preciseRounding)), finalResultRounding);
-		System.out.println("mtEch : "+mtEch);
-		
+	private MathContext finalResultRounding = new MathContext(4, RoundingMode.HALF_UP);
+	private MathContext preciseRounding = new MathContext(20, RoundingMode.HALF_UP);
+
+	public BigDecimal calculTotalCost(BigDecimal txAnnuel, int nbEch, BigDecimal mtEmp) {
+
+		BigDecimal mtEch = calculMonthlyCost(txAnnuel, nbEch, mtEmp);
+
 		BigDecimal cost = mtEch.multiply(new BigDecimal(nbEch));
-		System.out.println("cost : "+cost);
-		
+		System.out.println("cost : " + cost);
+
 		return cost;
 
 	}
 
+	/**
+	 * 
+	 * @param txAnnuel
+	 * @param nbEch
+	 * @param mtEmp
+	 * @return
+	 */
+	public BigDecimal calculMonthlyCost(BigDecimal txAnnuel, int nbEch, BigDecimal mtEmp) {
+
+		BigDecimal txPer = txAnnuel.divide(new BigDecimal(12), finalResultRounding);
+		System.out.println("txPer : " + txPer);
+
+		BigDecimal divided = mtEmp.multiply(txPer, preciseRounding)
+				.multiply(BigDecimal.ONE.add(txPer).pow(nbEch, preciseRounding));
+		BigDecimal divisor = BigDecimal.ONE.subtract(BigDecimal.ONE.add(txPer).pow(-nbEch, preciseRounding));
+		BigDecimal mtEch = divided.divide(divisor, finalResultRounding);
+		System.out.println("mtEch : " + mtEch);
+
+		return mtEch;
+
+	}
+
+	
+	public void afficherEcheancier(BigDecimal txAnnuel, int nbEch, BigDecimal mtEmp, LocalDate souscription) {
+		
+		BigDecimal mtEch = calculMonthlyCost(txAnnuel, nbEch, mtEmp);
+		
+		System.out.println("Date         Montant            Montant cumulé");
+		for (int i = 0; i< nbEch; i++) {
+			System.out.println(souscription.plusMonths(i)+"     "+mtEch+"     "
+		+(mtEch.add(mtEch.multiply(new BigDecimal(i)))));
+			
+		}
+		
+	}
+	
 }
