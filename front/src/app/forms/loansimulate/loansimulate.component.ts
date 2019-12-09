f62bc272c4de740538d31212975c4bd8262779e0
RAZAFIMANJATO <samoela.razafi@gmail.com>
2019-12-09 14:29:23 +0100

    modif loansimulate ( en cours)

--- front/src/app/forms/loansimulate/loansimulate.component.ts
+++ front/src/app/forms/loansimulate/loansimulate.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-loansimulate',
-  templateUrl: './loansimulate.component.html',
-  styleUrls: ['./loansimulate.component.scss']
+  selector: "app-loansimulate",
+  templateUrl: "./loansimulate.component.html",
+  styleUrls: ["./loansimulate.component.scss"]
 })
 export class LoansimulateComponent implements OnInit {
+  lastUpdate = new Date();
+
+  ops = [
+    {
+      date: this.lastUpdate,
+      montant: 376,
+      montantCumule: 376
+    }
+  ];
 
   constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
