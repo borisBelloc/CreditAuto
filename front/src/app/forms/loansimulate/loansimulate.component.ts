d7f45ff0b84d58de8d0225f58c3005fcec1dd25b
RAZAFIMANJATO <samoela.razafi@gmail.com>
2019-12-10 14:53:59 +0100

    modification *

--- front/src/app/forms/loansimulate/loansimulate.component.ts
+++ front/src/app/forms/loansimulate/loansimulate.component.ts
@@ -3,33 +3,33 @@ import { Component, OnInit } from "@angular/core";
 // Angular's FormBuilder service provides convenient methods for generating controls (FORMS)
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Simulation } from "../class/simulation";
-import { Observable, Subject, BehaviorSubject, merge } from 'rxjs';
-import { SimulationService } from '../service/simulation.service';
+import { Observable, Subject, BehaviorSubject, merge } from "rxjs";
+import { SimulationService } from "../service/simulation.service";
 
 @Component({
   selector: "app-loansimulate",
   templateUrl: "./loansimulate.component.html",
   styleUrls: ["./loansimulate.component.scss"]
 })
-
 export class LoansimulateComponent implements OnInit {
   dataSimulate: string;
   lastUpdate = new Date();
   submitted = false;
   simulateForm: FormGroup;
 
-  
   // Inject service to create Loan (forms)
-  constructor(private formBuilder: FormBuilder, private simulationService: SimulationService
-  ) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private simulationService: SimulationService
+  ) {}
 
   ngOnInit() {
     this.simulateForm = this.formBuilder.group({
-      amountPurchase: ['', Validators.required],
-      amountLoan: ['', Validators.required],
-      category: ['A', Validators.required],
-      durationLoan: ['', Validators.required],
-      loanCost: ['', Validators.required]
+      amountPurchase: ["", Validators.required],
+      amountLoan: ["", Validators.required],
+      category: ["A", Validators.required],
+      durationLoan: ["", Validators.required],
+      loanCost: ["", Validators.required]
     });
   }
 
@@ -42,6 +42,7 @@ export class LoansimulateComponent implements OnInit {
     console.log(this.simulateForm.value);
     console.log(this.dataSimulate);
     this.submitted = true;
+
     this.simulationService.postSimulation(this.simulateForm.value);
     // stop here if form is invalid
     if (this.simulateForm.invalid) {
@@ -55,5 +56,4 @@ export class LoansimulateComponent implements OnInit {
     //this.simulateForm.enable();
   }
   // this.simulateForm.disable();
-
 }
