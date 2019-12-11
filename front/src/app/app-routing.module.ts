544981fc5210e9250e793352375597c1b2050d5a
Boris BELLOC <bb.itwork@gmail.com>
2019-12-11 12:04:21 +0100

    added home routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoansimulateComponent } from './forms/loansimulate/loansimulate.component';
 import { CustomerComponent } from './forms/customer/customer.component';
+import { HomeComponent } from './body/home/home.component';
 
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: 'accueil',
-  //   pathMatch: 'full'
-  //   }
-  { path: '', redirectTo: 'simulation', pathMatch: 'full' },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+
+
   { path: 'simulation', component: LoansimulateComponent },
+
   { path: 'client', component: CustomerComponent },
   // TODO remove client2; used for test
   { path: 'client2', component: CustomerComponent }
