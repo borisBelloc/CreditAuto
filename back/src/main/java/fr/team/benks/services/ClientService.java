f8e4f11a77f481f1fbda18b92ab53141e6e07659
Nicolas <nicolas.gamel12450@gmail.com>
2019-12-11 10:36:27 +0100

    ajout du controller des clients

---
+++ back/src/main/java/fr/team/benks/services/ClientService.java
@@ -0,0 +1,16 @@
+package fr.team.benks.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import fr.team.benks.model.Client;
+import fr.team.benks.model.Rate;
+
+public class ClientService {
+
+	public List<Client> getRate(String nom, String prenom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
