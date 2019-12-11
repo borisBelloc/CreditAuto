3b81446f7d82f80fe170038d29ddfd3585874cb9
Nicolas <nicolas.gamel12450@gmail.com>
2019-12-11 14:52:23 +0100

    ajout du service permettant de verifier le mot de passe

--- back/src/main/java/fr/team/benks/model/User.java
+++ back/src/main/java/fr/team/benks/model/User.java
@@ -46,4 +46,52 @@ public class User implements IdEntity{
 		
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 }
