c093944eee3ddb9344a98c13ba31934aedeb15d7
Nicolas <nicolas.gamel12450@gmail.com>
2019-12-11 10:28:54 +0100

    ajout d'un controller pour les contrats

--- back/src/main/java/fr/team/benks/controllers/LoanController.java
+++ back/src/main/java/fr/team/benks/controllers/LoanController.java
@@ -5,13 +5,18 @@ import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.team.benks.model.LoanContract;
+import fr.team.benks.model.Rate;
 import fr.team.benks.services.CalculService;
+import fr.team.benks.services.LoanService;
 
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -19,7 +24,24 @@ import fr.team.benks.services.CalculService;
 @RequestMapping("/api/loans")
 public class LoanController {
 
+	@Autowired
+	private LoanService ls;
+	
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public void create(@RequestBody Rate resource) {
 
+		ls.save(resource);
+
+	}
+	
+	@RequestMapping(value = "{number}", method = RequestMethod.GET)
+	@ResponseBody
+	public LoanContract findByNumber(@PathVariable int number) {
+
+		return ls.get(number);
+
+	}
 	
 }
 
---
+++ back/src/main/java/fr/team/benks/services/LoanService.java
@@ -0,0 +1,18 @@
+package fr.team.benks.services;
+
+import fr.team.benks.model.LoanContract;
+import fr.team.benks.model.Rate;
+
+public class LoanService {
+
+	public void save(Rate resource) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public LoanContract get(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
