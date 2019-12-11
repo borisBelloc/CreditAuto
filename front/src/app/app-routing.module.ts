0a187d16dc5d9d21ca2d4895ad0f77afa1276e99
Boris BELLOC <bb.itwork@gmail.com>
2019-12-11 13:11:01 +0100

    fixed router duplicate

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -12,7 +12,6 @@ const routes: Routes = [
 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
 
   { path: 'login', component: IdentificationComponent },
   { path: 'transition', component: TransitionscreenComponent },
