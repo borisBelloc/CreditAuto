77d6a41149ee96f6ad9d78532a3738d265413db0
Nicolas <nicolas.gamel12450@gmail.com>
2019-12-12 10:11:10 +0100

    merging

--- back/src/main/java/fr/team/benks/dao/UserDAO.java
+++ back/src/main/java/fr/team/benks/dao/UserDAO.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package fr.team.benks.dao;
 
 import javax.persistence.EntityManager;
@@ -34,32 +34,4 @@ public class UserDAO extends AbstractJpaRepository<User> {
 
 
 }
-=======
-package fr.team.benks.dao;
 
-import java.util.Optional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
-import org.springframework.stereotype.Repository;
-
-import fr.team.benks.model.CategorieVehicle;
-import fr.team.benks.model.Rate;
-import fr.team.benks.model.User;
-
-@Repository
-public class UserDAO extends AbstractJpaRepository<User> {
-
-	protected UserDAO() {
-		super(User.class);
-	}
-	
-	@PersistenceContext
-	private EntityManager entityManager;
-
-
-
-}
->>>>>>> 8e39c4c6837857e41e44629d5b332c4874aaca84
